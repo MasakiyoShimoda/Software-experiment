19b29321ce0d05463856de932ccce7ff1f1b0f84
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-10-19 14:52:46 +0900

    Create 2.c

---
+++ onkyou/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define F 440 //周波数
+#define LEN 2
+#define A 20000 //振幅
+#define SAMPLE_RATE 44100
+
+int main(int argc, const char * argv[]) {
+    double y, t;
+    
+    srandom( (unsigned int)time(NULL) );
+    
+    for (t = 0; t < LEN; t += 1.0 / SAMPLE_RATE) {
+        if((A * sin(2.0 * M_PI * F * t)) > 0)
+            y = A;
+        else
+            y = -A;
+        printf("%d\n%d\n"  ,  (short)y, (short)y);
+    }
+    
+    return 0;
+}
