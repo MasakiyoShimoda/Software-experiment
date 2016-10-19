31b44e1fbb4743870543402e55414d0b14675573
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-10-19 15:08:05 +0900

    Update 3.c

--- onkyou/3.c
+++ onkyou/3.c
@@ -12,11 +12,8 @@ int main(int argc, const char * argv[]) {
     int i = 0;
     
     for (t = 0; t < LEN; t += 1.0 / SAMPLE_RATE) {
-        i = 10000 * t;
-        if((A * sin(2.0 * M_PI * F * t)) > 0)
-            y = A - i;
-        else
-            y = -A + i;
+                x  = (A-t*A/LEN) * sin(2.0 * M_PI * F * t);
+                y =   (t*A/LEN) * sin(2.0 * M_PI * F * t);
         
         printf("%d\n%d\n",  (short)x, (short)y);
     }
