b4f4dd9de5ea0ebfa77c968031b8305e3abf5fe4
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-10-19 14:52:16 +0900

    Create 1.c

---
+++ onkyou/1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define F 440
+#define LEN 2
+#define A 20000 //振幅
+#define SAMPLE_RATE 44100
+
+int main(int argc, const char * argv[]) {
+    double y, t;
+
+    srandom( (unsigned int)time(NULL) );
+    
+    for (t = 0; t <   t += 1.0 / SAMPLE_RATE) {
+        y = A * sin(2.0 * M_PI * F * t);
+        printf("%d\n%d\n"  ,  (short)y, (short)y);
+    }
+    
+    return 0;
+}
